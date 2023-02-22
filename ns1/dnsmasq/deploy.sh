80226be4d76b3d8e2ae0cdb5ebf6e4bdb80db428
Dict Xiong <me@beardic.cn>
2023-02-22 19:57:52 +0800

    bug fix (Wed Feb 22 19:57:52 CST 2023)

--- ns1/dnsmasq/deploy.sh
+++ ns1/dnsmasq/deploy.sh
@@ -3,5 +3,5 @@ set -e
 THIS_DIR=$( cd "$( dirname "${BASH_SOURCE[0]:-${(%):-%x}}" )" && pwd )
 THIS_FILE=$(basename "${BASH_SOURCE}")
 
-echo cp "$THIS_DIR"/dnsmasq.conf /etc/dnsmasq.conf
+cp "$THIS_DIR"/dnsmasq.conf /etc/dnsmasq.conf
 
