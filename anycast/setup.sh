64ae6a0911ea2b9f6ce027b069d52035185c1156
Dict Xiong <me@beardic.cn>
2023-02-25 01:44:57 +0800

    anycast/setup.sh: improve notes

--- anycast/setup.sh
+++ anycast/setup.sh
@@ -5,7 +5,18 @@ setup_network()
 {
     docker network create -d macvlan --internal --subnet 172.21.123.72/29 --ipv6 --subnet fda5:adba:5953:53::0/64 --attachable dn42-anycast
     docker network create -d bridge --subnet 172.18.53.1/24 --ipv6 --subnet fd5f:1867:ac9e:63ab::1/64 dn42-nat
-    echo please add \" -A POSTROUTING -s fd5f:1867:ac9e:63ab::1/64 -j MASQUERADE \" to /etc/ufw/before6.rules
+    echo please add the following lines to /etc/ufw/before6.rules:
+    cat <<EOF
+*nat
+:POSTROUTING ACCEPT [0:0]
+-A POSTROUTING -s fd5f:1867:ac9e:63ab::1/64 -j MASQUERADE
+COMMIT
+EOF
+    echo "add you may need to excute:"
+    cat <<EOF
+ufw route allow from fd5f:1867:ac9e:63ab::/64
+ufw route allow to fd5f:1867:ac9e:63ab::/64
+EOF
 }
 
 rm_network()
